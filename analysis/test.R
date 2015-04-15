0031185a46028e51a7dc7f76c906abc6b5cfcf93
Henri Terho <henri.terho@gmail.com>
2015-04-15 11:55:32 +0300

    added comments.

--- analysis/test.R
+++ analysis/test.R
@@ -12,12 +12,14 @@ install.packages("plyr")
 library(plyr)
 
 ue3 <- read.csv("../data/lupapiste-usage-events-all-20150414.tsv", sep = "\t", row.names = NULL)
+#convert date to POSIX format
 ue3$datetime <- strftime(ue$datetime, "%Y-%m-%d %H:%M:%OS3")
+#order list 
 ue3 <- ue3[with(ue3, order(applicationId, datetime)), ]
 
 
 
-#create table from data and calc
+#create table from data and count applicationId vs. actions
 table1 <- count(ue3, c("applicationId","action"))
 reshape(table1, v.names="freq", idvar="applicationId",timevar="action",direction="wide")
 
@@ -28,9 +30,6 @@ length(unique(ue3$applicationId))
 ue3max <- max(ue3$datetime)
 ue3min <- min(ue3$datetime)
 
-#create table from data and calc
-#table3 <- count(ue3, c("applicationId","action"))
-#reshape(table3, v.names="freq", idvar="applicationId",timevar="action",direction="wide")
 
 # a better way to do it...
 ue3AppIdVsAction <- table(ue3$applicationId, ue3$action )
