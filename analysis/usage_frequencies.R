b331bf0776b49311a2a13620ab35f4099f5f9220
Sampo Suonsyrjä <ssuonsyrja@gmail.com>
2015-04-21 12:49:19 +0300

    New function for checking if application is ok.

--- analysis/usage_frequencies.R
+++ analysis/usage_frequencies.R
@@ -1,11 +1,10 @@
 #install the plyr which contains the handy count function
 #install.packages("plyr")
-library(plyr)
-
-install.packages('data.table')
+#install.packages('data.table')
 
 # Import data into "ue" in RStudio or.. do the following
-#ue <- read.csv("../data/lupapiste-usage-events-all-20150414.tsv", sep = "\t", row.names = NULL)
+#setwd("\\\\intra.tut.fi/home/suonsyrj/My Documents/Publications/2015_ICIS/usage/analysis")
+ue <- read.csv("../data/lupapiste-usage-events-all-20150414.tsv", sep = "\t", row.names = NULL)
 
 #convert date to POSIX format
 ue$datetime <- strftime(ue$datetime, "%Y-%m-%d %H:%M:%OS3")
@@ -22,5 +21,10 @@ ue <- subset(ue, select = -c(action,target) )
 library(data.table)
 ueAppIdVsAction <- data.table(ue$applicationId, ue$Action )
 setnames(ueAppIdVsAction, 1:2, c("applicationId", "Action"))
+library(plyr)
 uf <- count(ueAppIdVsAction, c("applicationId","Action"))
 uf <- reshape(uf, v.names="freq", idvar="applicationId",timevar="Action",direction="wide")
+
+# Check the original data by applicationIDs and mark if they are ok.
+	# Choose events by appIds and send them in those chunks to "isApplicationOK"-func (in utils.R)
+	# Save these returned booleans by appIds
