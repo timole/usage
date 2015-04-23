2e8da69bf3d7425029efaca3578a1d2cc89f9b9a
Timo Lehtonen <timo.lehtonen@gmail.com>
2015-04-23 15:53:09 +0300

    added create event finding and unit tests

--- analysis/utils2.R
+++ analysis/utils2.R
@@ -1,12 +1,39 @@
+findCreateApplicationEvent <- function(ue, ae) {
+  # assuming ae is sorted by datetime ascending
+
+  earliest = ae[1,]
+  allCreatesBySameUser = ue[ue$userId == earliest$userId & ue$action == "create-application",]
+  allCreatesBySameUser <- allCreatesBySameUser[with(allCreatesBySameUser, order(datetime, decreasing = T)),]
+  latestCreate <- head(allCreatesBySameUser[allCreatesBySameUser$datetime < earliest$datetime,], 1)
+  if(nrow(latestCreate) == 1) {
+    latestCreate$applicationId <- earliest$applicationId
+  } else {
+    print(sprintf("warning: no create event found for application %d", earliest$applicationId))
+  }
+
+  return(latestCreate)
+}
+
+getApplicationEvents <- function(ue, applicationId) {
+  ue <- ue[!is.na(ue$applicationId) & ue$applicationId == applicationId,]
+}
+
 fixAndSortUsageEventData <- function(ue) {
   ue$datetime <- as.POSIXct(ue$datetime)
-  ue <- ue[!is.na(ue$applicationId),]
+  
+  applicationsUsageEvents <- split(ue , f = ue$applicationId)
+
+  createEvents <- lapply(names(applicationsUsageEvents), function(applicationId) {
+    events <- applicationsUsageEvents[applicationId][[1]]
+    created <- findCreateApplicationEvent(ue, events)
+    ue <<- rbind(ue, created)
+    return(created)
+  })
+
+  ue <- ue[!is.na(ue$applicationId ),]
   ue <- ue[with(ue, order(applicationId, datetime)), ]
 
-  # Merge action and target columns into one Action column
-  ue$Action <- paste(ue$action, ue$target)
-  ue[, "Action"] <- ue$Action
-  ue <- subset(ue, select = -c(action,target) )
+  return(ue)
 }
 
 # Usage events of one application as a parameter.
@@ -52,20 +79,24 @@ isApplicationOK <- function(data){
 # Usage events of one application as a parameter.
 # Returns the lead time of the application
 appLeadtime <- function(data){
-
-  data$datetime <- as.POSIXct(data$datetime)
-  
   # Order events by time.
   data <- data[with(data, order(datetime)), ]
+
+print(data)
   
   # Store the time of the first usage event.
   firstEvent <- data[1,"datetime"]
   
   # Find the row which has the approval event.
-  lastEvent <- data[which(data$Action == "publish-verdict " | data$Action == "approve-application "), "datetime"]
+  lastEvent <- data[which(data$action == "publish-verdict" | data$action == "approve-application"), "datetime"]
   
   # If there was no verdict published, return with NA.
   # TODO
+
+print("le")
+print(lastEvent)
+print("fe")
+print(firstEvent)
   
   # Return leadtime
   leadTime <- difftime(lastEvent, firstEvent)
--- test/utils2-test.R
+++ test/utils2-test.R
@@ -3,11 +3,14 @@ library("plyr")
 
 source("../analysis/utils2.R")
 
+clearLog()
+
 # sample data: 
 #   application 100 is ok and approved
 #   application 101 fails and then approved
-sue <- read.csv("../test/sampleUsageEvents.tsv", sep = "\t", row.names = NULL)
+sue <- read.csv("../test/sampleUsageEvents.tsv", sep = ";", row.names = NULL)
 sue <- fixAndSortUsageEventData(sue)
+sue
 
 print(sprintf("There are %d applications in the sample data", length(unique(sue$applicationId))))
 print(sue)
@@ -23,6 +26,20 @@ test(appLeadtime) <- function() {
 
 # TODO: create a test for function isApplicationOK
 test(isApplicationOK) <- function() {
+  DEACTIVATED()
   checkEqualsNumeric(123, 99999999)
 }
 (runTest(isApplicationOK))
+
+test(getApplicationEvents) <- function() {
+  ae <- getApplicationEvents(sue, 100)
+  checkEquals(nrow(ae), 9, "the amount of events for the application is known")
+
+  ae <- getApplicationEvents(sue, 101)
+  checkEqualsNumeric(nrow(ae), 11, "the amount of events for the application is known")
+}
+(runTest(getApplicationEvents))
+
+
+print("###################################### Summary #########################################")
+errorLog()
